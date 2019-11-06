963d5517751c9b3747946655838e7c87de195ca0
prosper <oviahonprosperpman32@gmail.com>
2019-11-06 10:45:47 -0800

    This is our first salesapp

---
+++ main.js
@@ -0,0 +1,22 @@
+function pay() {
+    var MOP = document.getElementById("ball").value;
+    document.getElementById("CAP").innerHTML= MOP;
+
+    var MOP = document.getElementById("sell").value;
+    document.getElementById("MOP").innerHTML= MOP;
+
+    var MOP = document.getElementById("amt").value;
+    document.getElementById("amount").innerHTML= MOP;
+
+    var MOP = document.getElementById("comment").value;
+    document.getElementById("dist").innerHTML= MOP;
+    
+    MOP.push()
+    document.getElementById("man").innerHTML = MOP;
+
+    
+    var str = "milk";
+    document.getElementById("dist").innerHTML= String.repeat(3);
+    
+    }
+    

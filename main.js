410b50f8344d8712869a92facc04975b698eebcb
prosper <oviahonprosperpman32@gmail.com>
2019-11-11 15:02:49 +0100

    ready to push to our github repo

--- main.js
+++ main.js
@@ -10,13 +10,17 @@ function pay() {
 
     var MOP = document.getElementById("comment").value;
     document.getElementById("dist").innerHTML= MOP;
-    
-    MOP.push()
-    document.getElementById("man").innerHTML = MOP;
 
-    
-    var str = "milk";
-    document.getElementById("dist").innerHTML= String.repeat(3);
-    
+var num ="";
+var copy = "CAP";
+var numm = "dist";
+var amt = "amount";
+var mop = "MOP";
+
+var serial = [];
+serial.push({Number:numm,category:copy,amount:amt,MOP:mop});
+for(i=0; i < serial.length; i++){
+    num +="<tr><td>" + Number(i+1) + "</td><td>"+serial[i].category+"</td><td>"+serial[i].amount+"</td><td>"+serial[i].MOP+"</td></tr>";
     }
-    
+    document.getElementById("demy").innerHTML = num;
+}
